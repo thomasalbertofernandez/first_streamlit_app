84f95a7cc34afd5f7288a7f44f4376a7e4f123f3
thomasalbertofernandez <136105391+thomasalbertofernandez@users.noreply.github.com>
2023-06-09 12:10:58 -0400

    Update streamlit_app.py
    menu title and header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
-streamlit.title('Breakfast Favorites')
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-boiled free range egg')
